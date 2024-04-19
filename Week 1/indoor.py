5c01da725c9d3f89d3833cf109f3801e1e7c84e6
Sreeju <77568405+Sreeju7733@users.noreply.github.com>
2024-04-19 12:29:06 +0530

    Delete Week 1 directory

--- Week 1/indoor.py
+++
@@ -1,5 +0,0 @@
-# getting input from user and converting to lowercase
-user_input = input().lower()
-
-# print the result
-print(user_input)
--- Week 1/playback.py
+++
@@ -1,5 +0,0 @@
-# getting input and replacing " " with "..."
-user_input = input().replace(" ", "...")
-
-# print the result
-print(user_input)
